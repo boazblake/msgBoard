a4c74dd2af3e0a1a17d8d6527e594a8db5597a47
boazblake <boazblake@gmail.com>
2016-06-17 14:39:00 -0500

    some styleing and ui functionality

---
+++ src/components/removeTaskButton.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export class RemoveTaskButton extends React.Component {
+  render(){
+    return(
+    <button className="btn btn-danger">X</button>
+    )
+  }
+}
