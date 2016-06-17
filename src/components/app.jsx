e02a0d679373fa84516144e5286f5f806e5845a0
boazblake <boazblake@gmail.com>
2016-06-17 11:57:41 -0500

    initial commit

---
+++ src/components/app.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends React.Component {
+  render(){
+    return (
+      <div>
+      <h2>The structure of the app</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
